343db6efe0775722a2dc828b4002430474421838
Milos Danilov <milos.danilov@sbgenomics.com>
2020-07-15 21:55:19 +0200

    chore(lib): remove default template service, component..

--- projects/angular-bootstrap/src/public-api.ts
+++ projects/angular-bootstrap/src/public-api.ts
@@ -2,6 +2,3 @@
  * Public API Surface of angular-bootstrap
  */
 
-export * from './lib/angular-bootstrap.service';
-export * from './lib/angular-bootstrap.component';
-export * from './lib/angular-bootstrap.module';
