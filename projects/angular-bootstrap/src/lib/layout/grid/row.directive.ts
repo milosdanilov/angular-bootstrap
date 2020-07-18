57214e92de7cc99481f9dcaf6d2e74470673f27f
Milos Danilov <milos.danilov@sbgenomics.com>
2020-07-18 22:38:03 +0200

    feat(lib): add bs-row for row selector

--- projects/angular-bootstrap/src/lib/layout/grid/row.directive.ts
+++ projects/angular-bootstrap/src/lib/layout/grid/row.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostBinding } from '@angular/core';
 
 @Directive({
-  selector: '[bsRow]'
+  selector: '[bsRow], [bs-row]'
 })
 export class RowDirective {
   @HostBinding('class') row = 'row';
