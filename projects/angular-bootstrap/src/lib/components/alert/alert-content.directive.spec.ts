a7849f80f4f3178299f9f402768ffa0cf6f07f7d
Milos Danilov <milos.danilov@sbgenomics.com>
2020-09-12 19:51:47 +0200

    refactor(lib alert): remove unnecessary spec for directive

--- projects/angular-bootstrap/src/lib/components/alert/alert-content.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AlertContentDirective } from './alert-content.directive';
-
-describe('AlertContentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AlertContentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
